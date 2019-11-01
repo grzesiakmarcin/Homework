ae1ebcaf6c8e66561189f04f64cfaa07d41e1591
Marcin Grzesiak <mk.grzesiak@gmail.com>
2019-11-02 00:06:08 +0100

    PrintWriter

--- src/PracaZPlikiem/opcja1/Main.java
+++ src/PracaZPlikiem/opcja1/Main.java
@@ -1,12 +1,18 @@
 package PracaZPlikiem.opcja1;
 
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) throws FileNotFoundException {
+
+
+        PrintWriter zapis = new PrintWriter("WszystkichSwietych.txt");
+        zapis.println("Jerzy Kukuczka - 24.10.1989 połódniowa ściana Lhotse ");
+        zapis.close();
+
+
     }
 
 
