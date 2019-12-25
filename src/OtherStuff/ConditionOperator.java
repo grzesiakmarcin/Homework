ca8aeab0a3ee1425d67f93d57299fd1e2bd92d1e
grzesiakmarcin <mk.grzesiak@gmail.com>
2019-12-25 12:04:30 +0100

    recode: String (makao==21)?"jest makao":"nie ma Makao";

--- src/OtherStuff/ConditionOperator.java
+++ src/OtherStuff/ConditionOperator.java
@@ -15,32 +15,9 @@ public class ConditionOperator {
         System.out.println("Podana liczba to:" + liczbaA);
 
 
+        String result= (liczbaA==21)?"Makao":"nie makao ;p";
+        System.out.println(result);
 
-        // na szybko
-
-//        if(liczbaA<20){
-//             liczbaA =21;
-//        }
-//        else if (liczbaA==20){
-//            liczbaA = 21;
-//                    }
-//        else{
-//            System.out.println(liczbaA);
-//        }
-//
-//        System.out.println("wynik odejmowania " +liczbaA " i " + "20  będzie dodatni i wyniesie "+(liczbaA-20) + "."  );
-
-/*
-od razu widac co jest grane i co robimy wiec po co to zmieniac operatorem warunkowym??
--mniej kodu na ekranie
-- zwiezly zapis (o ile nie zapomnisz co jest grane  <warunek> ? <wariant jeżeli  nbtak> <wariant jezeli nie>)
-
-
-
- */
-        int liczbaB = liczbaA <21 ? 21: liczbaA;  // <- jeżeli liczba jest więszka od  21 to wstaw wartość wskazaną przez użytkownika, , w innym razie wstaw 21
-
-        System.out.println("liczba B to: " +liczbaB);
 
 
     }
